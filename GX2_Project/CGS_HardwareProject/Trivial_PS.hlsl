47d54e052934ebd9a71bdaf89776febbbea4e1d7
Jonah Rogers <jsrogers@fullsail.edu>
2018-04-11 01:47:02 -0400

    cube rotating around cube
    zbuffer added
    index buffer added
    camera movement added

--- GX2_Project/CGS_HardwareProject/Trivial_PS.hlsl
+++ GX2_Project/CGS_HardwareProject/Trivial_PS.hlsl
@@ -1,4 +1,35 @@
-float4 main( float4 colorFromRasterizer : COLOR ) : SV_TARGET
+//struct INPUT_PIXEL
+//{
+//	float4 coordinate : SV_POSITION;
+//	float3 normal : NORMAL;
+//};
+//
+//cbuffer LightBuffer
+//{
+//	float4 ambientColor;
+//	float4 diffuseColor;
+//	float3 lightDirection;
+//	float padding;
+//};
+
+float4 main(/*INPUT_PIXEL*/float4 colorFromRasterizer : COLOR) : SV_TARGET
 {
+	/*float3 lightDir;
+	float lightIntensity;
+	float4 color;
+
+	color = ambientColor;
+
+	lightDir = -lightDirection;
+
+	lightIntensity = saturate(dot(colorFromRasterizer.normal, lightDir));
+
+	if (lightIntensity > 0.0f)
+	{
+		color += (diffuseColor * lightIntensity);
+	}
+
+	color = saturate(color);*/
+
 	return colorFromRasterizer;
 }
--- GX2_Project/CGS_HardwareProject/Trivial_VS.hlsl
+++ GX2_Project/CGS_HardwareProject/Trivial_VS.hlsl
@@ -2,43 +2,66 @@
 
 struct INPUT_VERTEX
 {
-	float3 coordinate : POSITION;
+	float4 coordinate : POSITION;
+	float4 color : COLOR;
+	//float3 normal : NORMAL;
 };
 
 struct OUTPUT_VERTEX
 {
 	float4 colorOut : COLOR;
 	float4 projectedCoordinate : SV_POSITION;
+	//float3 normal : NORMAL;
 };
+
+//struct NF3D_LIGHT_OMNIDIRECTIONAL
+//{
+//	float4 Diffuse;
+//	float4 Ambient;
+//	float3 Position;
+//	float3 Attitude;
+//	float Range;
+//	int BindSlot;
+//};
 
 // TODO: PART 3 STEP 2a
 cbuffer THIS_IS_VRAM : register(b0)
 {
 	float4x4 worldMatrix;
+	float4x4 viewMatrix;
+	float4x4 projectionMatrix;
 
-	float4 constantColor;
+	//float4 constantColor;
 	//float2 constantOffset;
 	//float2 padding;
 };
 
 cbuffer SCENE : register(b1)
 {
-	float4x4 viewMatrix;
-	float4x4 projectionMatrix;
+	//NF3D_LIGHT_OMNIDIRECTIONAL light;
+	//float4x4 viewMatrix;
+	//float4x4 projectionMatrix;
 }
 
 OUTPUT_VERTEX main(INPUT_VERTEX fromVertexBuffer)
 {
 	OUTPUT_VERTEX sendToRasterizer = (OUTPUT_VERTEX)0;
-	sendToRasterizer.projectedCoordinate.w = 1;
+	//sendToRasterizer.projectedCoordinate.w = 1;
 
-	sendToRasterizer.projectedCoordinate.xy = fromVertexBuffer.coordinate.xy;
+	//sendToRasterizer.projectedCoordinate.xy = fromVertexBuffer.coordinate.xy;
+
+	sendToRasterizer.projectedCoordinate = mul(fromVertexBuffer.coordinate, worldMatrix);
+	sendToRasterizer.projectedCoordinate = mul(sendToRasterizer.projectedCoordinate, viewMatrix);
+	sendToRasterizer.projectedCoordinate = mul(sendToRasterizer.projectedCoordinate, projectionMatrix);
+	
+	//sendToRasterizer.normal = mul(fromVertexBuffer.normal, (float3x3)worldMatrix);
+	//sendToRasterizer.normal = normalize(sendToRasterizer.normal);
 
 	// TODO : PART 4 STEP 4
 	//sendToRasterizer.projectedCoordinate.xy += constantOffset;
 
 	// TODO : PART 3 STEP 7
-	sendToRasterizer.colorOut = constantColor;
+	sendToRasterizer.colorOut = fromVertexBuffer.color;
 	// END PART 3
 
 	return sendToRasterizer;
