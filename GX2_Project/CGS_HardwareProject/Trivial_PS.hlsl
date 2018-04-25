717c25966e6c0b56d2413d2f2328f33e4ee272c3
Jonah Rogers <jsrogers@fullsail.edu>
2018-04-25 00:52:13 -0400

    Having problems with git
    I was having a really hard time replacing my old project, that crashed randomly, with my new, fixed, project. But it's fixed.

--- GX2_Project/CGS_HardwareProject/Trivial_PS.hlsl
+++ GX2_Project/CGS_HardwareProject/Trivial_PS.hlsl
@@ -1,14 +1,21 @@
-//struct OUTPUT_VERTEX
-//{
-//	float4 coordinate : SV_POSITION;
-//	float2 tex : TEXCOORD0;
-//	//float4 color : COLOR;
-//	//float3 normal : NORMAL;
-//};
+struct OUTPUT_VERTEX
+{
+	float4 projectedCoordinate : SV_POSITION;
+	float3 normal : NORMAL;
+	float2 texOut : TEXCOORD0;
+	float4 worldPos : POSITION;
+};
 
 texture2D baseTexture : register(t0);
 
 SamplerState baseFilter : register(s0);
+
+//struct LIGHT
+//{
+//	float3 lightDirection;
+//	float4 lightPosition;
+//	float4 ambientColor;
+//};
 
 cbuffer THIS_IS_VRAM : register(b0)
 {
@@ -16,33 +23,41 @@ cbuffer THIS_IS_VRAM : register(b0)
 	float4x4 viewMatrix;
 	float4x4 projectionMatrix;
 
+	//LIGHT light[3];
 	float3 lightDirection;
-
+	float4 lightPosition;
 	float4 ambientColor;
+}
 
-	//float4 constantColor;
-	//float2 constantOffset;
-	//float2 padding;
-};
-
-float4 main( /*OUTPUT_VERTEX input*/float2 tex : TEXCOORD0/*, float3 normal : NORMAL*/) : SV_TARGET
+float4 main(OUTPUT_VERTEX vertex ) : SV_TARGET
 {
-	//float3 lightDir;
-	//float lightIntensity;
-	//float4 color;
-	//
-	//color = baseTexture.Sample(baseFilter, tex);
-	//
-	//lightDir = -lightDirection;
-	//
-	//lightIntensity = saturate(dot(normal, lightDir));
-	//
-	//if (lightIntensity > 0.0f)
-	//{
-	//	color += (ambientColor * lightIntensity);
-	//}
-	//
-	//color = saturate(color);
+	float4 color = baseTexture.Sample(baseFilter, vertex.texOut);
+	
+	float4 ambient = float4(0.2f, 0.2f, 0.2f, 1.0f);
 
-	return baseTexture.Sample(baseFilter, tex);;
+	//for directional light
+	float3 lightDir = normalize(-lightDirection);
+	float lightRatioDir = saturate(dot(lightDir, vertex.normal));
+	lightRatioDir = saturate(lightRatioDir + ambient);
+	color = lightRatioDir * ambientColor * color;
+
+	// for point light
+	//float3 lightPoint = (normalize(light[1].lightPosition - vertex.worldPos)).xyz;
+	//float lightRatioPoint = saturate(dot(lightPoint, vertex.normal));
+	//float attenuationPoint = 1.0 - saturate(length(light[1].lightPosition.xyz - vertex.worldPos.xyz) / 2.5f);
+	//lightRatioPoint = saturate((lightRatioPoint * attenuationPoint));// + ambient);
+	//float4 pointColor = lightRatioPoint * light[1].ambientColor;// *color;
+
+	//for spot light
+	//float3 lightSpot = normalize(light[2].lightPosition.xyz - vertex.worldPos.xyz);
+	//float surfaceRatio = saturate(dot(-lightSpot, lightDir));
+	//float spotFactor = (surfaceRatio > 20) ? 1 : 0;
+	//float lightRatioSpot = saturate(dot(lightSpot, vertex.normal));
+	//float attenuationSpot = 1.0 - saturate((0.6f - surfaceRatio) / (0.6f - 0.5f));
+	//lightRatioSpot = saturate((lightRatioSpot * attenuationSpot));// +ambient);
+	//float4 spotColor = surfaceRatio * lightRatioSpot * light[2].ambientColor;// * color;
+
+	color = saturate(color);
+
+	return color;
 }
--- GX2_Project/CGS_HardwareProject/Trivial_VS.hlsl
+++ GX2_Project/CGS_HardwareProject/Trivial_VS.hlsl
@@ -2,56 +2,56 @@
 
 struct INPUT_VERTEX
 {
-<<<<<<< HEAD
 	float3 coordinate : POSITION;
-=======
-	float4 coordinate : POSITION;
-	float4 color : COLOR;
-<<<<<<< HEAD
->>>>>>> parent of 0f18ab0... Working on textures
-=======
->>>>>>> parent of 0f18ab0... Working on textures
-	//float3 normal : NORMAL;
+	float3 normal : NORMAL;
 	float2 tex : TEXCOORD0;
-	//float4 color : COLOR;
 };
 
 struct OUTPUT_VERTEX
 {
-<<<<<<< HEAD
+	float4 projectedCoordinate : SV_POSITION;
+	float3 normal : NORMAL;
 	float2 texOut : TEXCOORD0;
-=======
-	float4 colorOut : COLOR;
-	float4 projectedCoordinate : SV_POSITION;
-<<<<<<< HEAD
->>>>>>> parent of 0f18ab0... Working on textures
-=======
->>>>>>> parent of 0f18ab0... Working on textures
-	//float3 normal : NORMAL;
-	float4 projectedCoordinate : SV_POSITION;
-	//float4 colorOut : COLOR;
+	float4 worldPos : POSITION;
 };
 
+//struct LIGHT
+//{
+//	float3 lightDirection;
+//	float4 lightPosition;
+//	float4 ambientColor;
+//};
+
+// TODO: PART 3 STEP 2a
 cbuffer THIS_IS_VRAM : register(b0)
 {
 	float4x4 worldMatrix;
 	float4x4 viewMatrix;
 	float4x4 projectionMatrix;
-	
-	//float3 lightDirection;
-	//float4 ambientColor;
+
+	//LIGHT light[3];
+	float3 lightDirection;
+	float4 lightPosition;
+	float4 ambientColor;
 };
+
+cbuffer SCENE : register(b1)
+{
+	//NF3D_LIGHT_OMNIDIRECTIONAL light;
+	//float4x4 viewMatrix;
+	//float4x4 projectionMatrix;
+}
 
 OUTPUT_VERTEX main(INPUT_VERTEX fromVertexBuffer)
 {
 	OUTPUT_VERTEX sendToRasterizer = (OUTPUT_VERTEX)0;
 
-	sendToRasterizer.projectedCoordinate = mul(float4(fromVertexBuffer.coordinate, 1), worldMatrix);
-	sendToRasterizer.projectedCoordinate = mul(sendToRasterizer.projectedCoordinate, viewMatrix);
+	sendToRasterizer.worldPos = mul(float4(fromVertexBuffer.coordinate, 1), worldMatrix);
+	sendToRasterizer.projectedCoordinate = mul(sendToRasterizer.worldPos, viewMatrix);
 	sendToRasterizer.projectedCoordinate = mul(sendToRasterizer.projectedCoordinate, projectionMatrix);
 	
-	//sendToRasterizer.normal = mul(fromVertexBuffer.normal, (float3x3)worldMatrix);
-	//sendToRasterizer.normal = normalize(sendToRasterizer.normal);
+	sendToRasterizer.normal = mul(fromVertexBuffer.normal, (float3x3)worldMatrix);
+	sendToRasterizer.normal = normalize(sendToRasterizer.normal);
 
 	sendToRasterizer.texOut = fromVertexBuffer.tex;
 
